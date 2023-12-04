2567ef612aed4b21ad04d2596a160c02a68ea2eb
Galekwan Sango <galekwansango@gmail.com>
2023-12-04 14:57:23 -0500

    registration is working

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"; 
 import { AuthProvider } from './context/AuthPorvider.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
